201ad214540e1eb4f8eb2bbb9d7dbfefc5478744
crisgon <kristiano.gon@gmail.com>
2017-08-17 20:49:37 -0300

    Script que carrega o valor das moedas. [Finalizado]

--- js/main.js
+++ js/main.js
@@ -2,13 +2,29 @@ var $ = function(name){
 	return document.querySelector(name);
 };
 
-var lis = document.querySelectorAll("li");
-// var dolar = $(".dolar").getElementsByTagName("span");
-// var euro = $(".euro").getElementsByTagName("span");
-// var peso = $(".peso").getElementsByTagName("span");
-// var bitcoin = $(".bitcoin").getElementsByTagName("span");
+//Captura elementos
+var container = $(".container");
+var dolar = $(".valor-dolar");
+var euro  = $(".valor-euro");
+var peso  = $(".valor-peso");
+var libra = $(".valor-libra");
+var teste = $(".teste");
 
-lis.forEach(function(indice){
-	console.log(indice.getElementsByTagName("span"));
-	
+
+//Faz conexão e retorna o JSON
+var conexao = new XMLHttpRequest();
+conexao.open("GET", "http://api.promasters.net.br/cotacao/v1/valores"); //abre uma conexão 
+
+conexao.addEventListener("load", function(){
+		var conteudoJson = JSON.parse(conexao.responseText); //converte o texto para um objeto js
+		console.log(conteudoJson);
+		dolar.innerHTML = conteudoJson.valores.USD.valor.toFixed(2);
+		euro.innerHTML  = conteudoJson.valores.EUR.valor.toFixed(2);
+		peso.innerHTML  = conteudoJson.valores.ARS.valor.toFixed(2);
+		libra.innerHTML = conteudoJson.valores.GBP.valor.toFixed(2);
+		teste.classList.add("fadeOut");
+		container.classList.add("fadeIn");
 });
+	conexao.send(); // envia
+
+	console.log(conexao.status);
